edca5802ff0d4877a20ab179f001d74ed9ac645b
Natalya-Tester <natali30071983@yandex.ru>
2023-09-07 08:28:59 +0100

    SQL_Autotest_Version_1

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://41e61ef0-0858-46be-a3e4-83ff16fa2aec.serverhub.praktikum-services.ru"  # адрес сервера
+CREATE_COURIER = "/api/v1/courier"  # создание курьера
+CREATE_ORDER = "/api/v1/orders"  # создание заказа
+ORDER_INFO = "/api/v1/orders/track?t="  # получить информацию о заказе по его номеру
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Елисеев",
+    "lastName": "Елисей",
+    "address": "Ленина, 15",
+    "metroStation": 10,
+    "phone": "+7 915 458 36 17",
+    "rentTime": 3,
+    "deliveryDate": "2023-09-07",
+    "comment": "Прокачусь с ветерком",
+    "color": [
+        "BLACK"
+    ]
+}
